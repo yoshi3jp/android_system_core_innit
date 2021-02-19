master-hax/aosp-system-core

init: ro.boottime.init.modules
Add a property ro.boottime.init.modules to provide kernel modules
loading time in milliseconds. Also add corresponding log to show in init
log along with loaded module count.

Test: boot test
Bug: 178143513
Change-Id: I77e3939c2a271da6841350a8c2a34ad32f637377

init/first_stage_init.h
@@ -22,6 +22,7 @@ namespace init {
 int FirstStageMain(int argc, char** argv);
 
 static constexpr char kEnvFirstStageStartedAt[] = "FIRST_STAGE_STARTED_AT";
+static constexpr char kEnvInitModuleStartedAt[] = "INIT_MODULE_STARTED_AT";
 
 }  // namespace init
 }  // namespace android

yoshi3jp/android_system_core_innit

init/first_stage_init.h
@@ -22,6 +22,7 @@ namespace init {
 int FirstStageMain(int argc, char** argv);
 
 static constexpr char kEnvFirstStageStartedAt[] = "FIRST_STAGE_STARTED_AT";
+static constexpr char kEnvInitModuleDurationMs[] = "INIT_MODULE_DURATION_MS";
 
 }  // namespace init
 }  // namespace android